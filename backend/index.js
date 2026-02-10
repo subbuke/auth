bf0d2918951e4eefa52017e4ba45b2fcd0cc3ef3
subramanyam <141896120+subbuke@users.noreply.github.com>
2026-02-10 15:40:08 +0000

    project started

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
-    res.send("hello world");
+    res.send("working")
 })
 
-const port = process.env.port || 4000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log("server is running");
+app.listen(PORT, () => {
+    console.log("server is running")
 })
