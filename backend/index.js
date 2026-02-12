e244e906030bfeeb2690e9857316529e9e162065
subramanyam <141896120+subbuke@users.noreply.github.com>
2026-02-12 10:50:53 +0000

    project runing

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,14 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const UserModel = require("./Model")
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-mongoose.connect("mongodb+srv://subramanyamchowdam7654:subbu1919@cluster1.0ybx9.mongodb.net/admin123?appName=Cluster1")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log("database connected")
 }).catch(err => console.log(err))
